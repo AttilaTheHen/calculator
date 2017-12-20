19c424d0b0b19f8ec30ec3a5cf57aaaab860e786
injoongyoon <injoongyoon@gmail.com>
2017-12-20 13:48:28 -0800

    Make the display show subsequent numbers properly

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 function add(numOne, numTwo) {
   return numOne + numTwo;
 }
-
 function subtract(numOne, numTwo) {
   return numOne - numTwo;
 }
-
 function multiply(numOne, numTwo) {
   return numOne * numTwo;
 }
-
 function divide(numOne, numTwo) {
   return numOne / numTwo;
 }
-
 function operate(numOne, numTwo, operator) {
   return operator(numOne, numTwo);
 }
+function displayNum(e) {
+  num += e.target.textContent;
+  display.value = num;
+}
+function clearNum() {
+  while (display.value) {
+    display.value = '';
+  }
+}
+
+let display = document.querySelector('input');
+let numOne = '';
+let numTwo = '';
+let num = '';
+
+const buttons = document.getElementsByClassName('num');
+for (let i = 0; i < buttons.length; i++) {
+  const test = buttons[i];
+  test.addEventListener('click', displayNum);
+} // This block of code attaches a click event to each number button on the calculator.
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', clearNum);
