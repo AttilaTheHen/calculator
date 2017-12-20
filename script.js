89d77edbfee365cc52db51c91b50652e452e38e9
injoongyoon <injoongyoon@gmail.com>
2017-12-20 14:50:26 -0800

    Add operator functionality so operator buttons all work properly

--- script.js
+++ script.js
@@ -20,19 +20,64 @@ function displayNum(e) {
 function clearNum() {
   while (display.value) {
     display.value = '';
+    num = '';
   }
+}
+
+function addNum() {
+  numOne = parseInt(num);
+  operator = add;
+  num = '';
+}
+function subNum() {
+  numOne = parseInt(num);
+  operator = subtract;
+  num = '';
+}
+function multNum() {
+  numOne = parseInt(num);
+  operator = multiply;
+  num = '';
+}
+function divNum() {
+  numOne = parseInt(num);
+  operator = divide;
+  num = '';
+}
+
+function equalNum() {
+  numTwo = parseInt(num);
+  let answer = operate(numOne, numTwo, operator);
+  display.value = answer;
+  num = '';
+  numOne = '';
+  numTwo = '';
 }
 
 let display = document.querySelector('input');
 let numOne = '';
 let numTwo = '';
 let num = '';
+let operator = '';
 
-const buttons = document.getElementsByClassName('num');
-for (let i = 0; i < buttons.length; i++) {
-  const test = buttons[i];
+const digits = document.getElementsByClassName('num');
+for (let i = 0; i < digits.length; i++) {
+  const test = digits[i];
   test.addEventListener('click', displayNum);
-} // This block of code attaches a click event to each number button on the calculator.
+} // This block of code (starting at 'const digits') attaches a click event to each number button on the calculator.
+// Basic order of figuring this out: attach click events to buttons so that a certain function runs when the right button is clicked, write functions that dictate what happens when the function runs
+
+const addition = document.querySelector('#add');
+addition.addEventListener('click', addNum);
+const subtraction = document.querySelector('#sub');
+subtraction.addEventListener('click', subNum);
+const multiple = document.querySelector('#mult');
+multiple.addEventListener('click', multNum);
+const division = document.querySelector('#divi');
+division.addEventListener('click', divNum);
+
+const calculate = document.querySelector('#calc');
+calculate.addEventListener('click', equalNum);
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', clearNum);
