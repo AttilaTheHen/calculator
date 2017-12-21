a717d815f5040d45a63f3120e7909dee37758cd1
injoongyoon <injoongyoon@gmail.com>
2017-12-21 10:55:17 -0800

    Remove comments, clean up spacing, make no other changes

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function divi(numOne, numTwo) {
 function operate(numOne, numTwo, operator) {
   return operator(numOne, numTwo);
 }
+
 function displayNum(e) {
   num += e.target.textContent;
   display.value = num;
@@ -23,26 +24,6 @@ function clearNum() {
     num = '';
   }
 }
-// function addNum() {
-//   numOne = parseInt(num);
-//   operator = add;
-//   num = '';
-// }
-// function subNum() {
-//   numOne = parseInt(num);
-//   operator = subtract;
-//   num = '';
-// }
-// function multNum() {
-//   numOne = parseInt(num);
-//   operator = multiply;
-//   num = '';
-// }
-// function divNum() {
-//   numOne = parseInt(num);
-//   operator = divide;
-//   num = '';
-// }
 function operateNum(e) {
   if (numOne) {
     numTwo = parseInt(num);
@@ -56,9 +37,6 @@ function operateNum(e) {
     operator = eval(this.id);
     num = '';
   }
-  // numOne = parseInt(num);
-  // operator = eval(this.id);
-  // num = '';
 }
 function equalNum() {
   numTwo = parseInt(num);
@@ -73,33 +51,19 @@ let display = document.querySelector('input');
 let numOne = '';
 let numTwo = '';
 let num = '';
-// let operator = '';
 let answer = '';
 
 const digits = document.getElementsByClassName('num');
 for (let i = 0; i < digits.length; i++) {
   const test = digits[i];
   test.addEventListener('click', displayNum);
-} // This block of code (starting at 'const digits') attaches a click event to each number button on the calculator.
-// Basic order of figuring this out: attach click events to buttons so that a certain function runs when the right button is clicked, write functions that dictate what happens when the function runs
-
+}
 const operations = document.getElementsByClassName('operator');
 for (let j = 0; j < operations.length; j++) {
   const opButton = operations[j];
   opButton.addEventListener('click', operateNum)
 }
-
-// const addition = document.querySelector('#add');
-// addition.addEventListener('click', operateNum(add));
-// const subtraction = document.querySelector('#sub');
-// subtraction.addEventListener('click', operateNum(subtract));
-// const multiple = document.querySelector('#mult');
-// multiple.addEventListener('click', operateNum(multiply));
-// const division = document.querySelector('#divi');
-// division.addEventListener('click', operateNum(divide));
-
 const calculate = document.querySelector('#calc');
 calculate.addEventListener('click', equalNum);
-
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', clearNum);
