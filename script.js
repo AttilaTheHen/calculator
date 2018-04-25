bb9c9c143bab84a729fd0cab24a81faafc328ee0
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-04-25 12:45:58 -0700

    Change single quotes to double

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
-let display = document.querySelector('input');
-let numOne = '';
-let numTwo = '';
-let num = '';
-let answer = '';
+let display = document.querySelector("input");
+let numOne = "";
+let numTwo = "";
+let num = "";
+let answer = "";
 let decKeyDisabled = false;
 let opKeyDisabled = true;
 let calcKeyDisabled = true;
-let operandButton = '';
-let numberButton = '';
-let dotButton = '';
+let operandButton = "";
+let numberButton = "";
+let dotButton = "";
 
-const digits = document.getElementsByClassName('num');
+const digits = document.getElementsByClassName("num");
 for (let j = 0; j < digits.length; j++) {
   const digit = digits[j];
-  digit.addEventListener('click', displayMouse);
+  digit.addEventListener("click", displayMouse);
 }
-const operations = document.getElementsByClassName('operator');
+const operations = document.getElementsByClassName("operator");
 for (let k = 0; k < operations.length; k++) {
   let opButton = operations[k];
-  opButton.addEventListener('click', operateNum);
+  opButton.addEventListener("click", operateNum);
 }
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', clearNum);
-const decimal = document.querySelector('#dec');
-decimal.addEventListener('click', displayMouse);
-const calculate = document.querySelector('#calc');
-calculate.addEventListener('click', equalNum);
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearNum);
+const decimal = document.querySelector("#dec");
+decimal.addEventListener("click", displayMouse);
+const calculate = document.querySelector("#calc");
+calculate.addEventListener("click", equalNum);
 disableButtons();
 calculate.disabled = true;
-const numButtons = document.querySelectorAll('.num, .use, .dot, .equals');
-numButtons.forEach(numButton => numButton.addEventListener('transitionend', removeTransition));
-document.addEventListener('keydown', useKey);
+const numButtons = document.querySelectorAll(".num, .use, .dot, .equals");
+numButtons.forEach(numButton =>
+  numButton.addEventListener("transitionend", removeTransition)
+);
+document.addEventListener("keydown", useKey);
 
 function useKey(e) {
   document.activeElement.blur();
-  if (!isNaN(e.key)) displayKey(e.key)
-  else if (e.key == '+') {
+  if (!isNaN(e.key)) displayKey(e.key);
+  else if (e.key == "+") {
     if (!opKeyDisabled) {
       operateNumKey(add);
       opKeyDisabled = true;
     }
-  } else if (e.key == '-') {
+  } else if (e.key == "-") {
     if (!opKeyDisabled) {
       operateNumKey(sub);
       opKeyDisabled = true;
     }
-  } else if (e.key == '*') {
+  } else if (e.key == "*") {
     if (!opKeyDisabled) {
       operateNumKey(mult);
       opKeyDisabled = true;
     }
-  } else if (e.key == '/') {
+  } else if (e.key == "/") {
     if (!opKeyDisabled) {
       operateNumKey(divi);
       opKeyDisabled = true;
     }
-  } else if (e.key == '.') {
+  } else if (e.key == ".") {
     if (!decKeyDisabled) {
       displayKey(e.key);
       decKeyDisabled = true;
     }
-  } else if (e.key == 'Backspace') {
+  } else if (e.key == "Backspace") {
     clearNum();
-  } else if (e.key == 'Enter') {
+  } else if (e.key == "Enter") {
     if (!calcKeyDisabled) {
       equalNum();
     }
-  } else if (e.key == '=') {
+  } else if (e.key == "=") {
     if (!calcKeyDisabled) {
       equalNum();
     }
@@ -88,14 +90,14 @@ function displayKey(e) {
   }
   num += e;
   display.value = num;
-  if (e == '.') {
+  if (e == ".") {
     decimal.disabled = true;
     decKeyDisabled = true;
-    dotButton = document.querySelector('#dec');
-    dotButton.classList.add('button-transition');
+    dotButton = document.querySelector("#dec");
+    dotButton.classList.add("button-transition");
   } else {
-    numberButton = document.querySelector('#' + 'b' + e);
-    numberButton.classList.add('button-transition');
+    numberButton = document.querySelector("#" + "b" + e);
+    numberButton.classList.add("button-transition");
   }
   enableButtons();
   opKeyDisabled = false;
@@ -103,16 +105,16 @@ function displayKey(e) {
 }
 
 function removeTransition() {
-  this.classList.remove('button-transition');
+  this.classList.remove("button-transition");
 }
 
 function clearNum() {
   while (display.value) {
-    display.value = '';
-    num = '';
-    numOne = '';
-    numTwo = '';
-    answer = '';
+    display.value = "";
+    num = "";
+    numOne = "";
+    numTwo = "";
+    answer = "";
   }
   disableButtons();
   opKeyDisabled = true;
@@ -120,21 +122,21 @@ function clearNum() {
   decimal.disabled = false;
   equalNum.called = false;
   decKeyDisabled = false;
-  let button = document.querySelector('#clear');
-  button.classList.add('button-transition');
+  let button = document.querySelector("#clear");
+  button.classList.add("button-transition");
   if (operandButton) {
-    operandButton.classList.remove('button-selected');
+    operandButton.classList.remove("button-selected");
   }
   calcKeyDisabled = true;
 }
 
 function softClear() {
   while (display.value) {
-    display.value = '';
-    num = '';
-    numOne = '';
-    numTwo = '';
-    answer = '';
+    display.value = "";
+    num = "";
+    numOne = "";
+    numTwo = "";
+    answer = "";
   }
   disableButtons();
   calculate.disabled = true;
@@ -148,26 +150,26 @@ function softClear() {
 function operateNumKey(e) {
   if (numOne) {
     if (equalNum.called) {
-      numTwo = '';
-      num = '';
+      numTwo = "";
+      num = "";
       operator = eval(e);
     } else {
       softEqual();
-      num = '';
-      numTwo = '';
+      num = "";
+      numTwo = "";
       operator = eval(e);
     }
     if (operandButton) {
-      operandButton.classList.remove('button-selected');
-      operandButton = document.querySelector('#' + e.name);
-      operandButton.classList.add('button-selected');
+      operandButton.classList.remove("button-selected");
+      operandButton = document.querySelector("#" + e.name);
+      operandButton.classList.add("button-selected");
     }
   } else {
     numOne = parseFloat(num);
     operator = eval(e);
-    num = '';
-    operandButton = document.querySelector('#' + e.name);
-    operandButton.classList.add('button-selected');
+    num = "";
+    operandButton = document.querySelector("#" + e.name);
+    operandButton.classList.add("button-selected");
   }
   disableButtons();
   decimal.disabled = false;
@@ -179,26 +181,26 @@ function operateNumKey(e) {
 function operateNum() {
   if (numOne) {
     if (equalNum.called) {
-      numTwo = '';
-      num = '';
+      numTwo = "";
+      num = "";
       operator = eval(this.id);
     } else {
       softEqual();
-      num = '';
-      numTwo = '';
+      num = "";
+      numTwo = "";
       operator = eval(this.id);
     }
     if (operandButton) {
-      operandButton.classList.remove('button-selected');
-      operandButton = document.querySelector('#' + this.id);
-      operandButton.classList.add('button-selected');
+      operandButton.classList.remove("button-selected");
+      operandButton = document.querySelector("#" + this.id);
+      operandButton.classList.add("button-selected");
     }
   } else {
     numOne = parseFloat(num);
     operator = eval(this.id);
-    num = '';
-    operandButton = document.querySelector('#' + this.id);
-    operandButton.classList.add('button-selected');
+    num = "";
+    operandButton = document.querySelector("#" + this.id);
+    operandButton.classList.add("button-selected");
   }
   disableButtons();
   decimal.disabled = false;
@@ -213,10 +215,10 @@ function equalNum() {
   answer = operate(numOne, numTwo, operator);
   if (numTwo == 0 && operator == divi) {
     display.value = "INFINITY!";
-    num = '';
-    numOne = '';
-    numTwo = '';
-    answer = '';
+    num = "";
+    numOne = "";
+    numTwo = "";
+    answer = "";
     disableButtons();
     calculate.disabled = true;
     equalNum.called = false;
@@ -235,9 +237,9 @@ function equalNum() {
   // equalNum.called = true;
   decimal.disabled = false;
   decKeyDisabled = false;
-  let button = document.querySelector('#calc');
-  button.classList.add('button-transition');
-  operandButton.classList.remove('button-selected');
+  let button = document.querySelector("#calc");
+  button.classList.add("button-transition");
+  operandButton.classList.remove("button-selected");
 }
 
 function softEqual() {
@@ -245,10 +247,10 @@ function softEqual() {
   answer = operate(numOne, numTwo, operator);
   if (numTwo == 0 && operator == divi) {
     display.value = "INFINITY!";
-    num = '';
-    numOne = '';
-    numTwo = '';
-    answer = '';
+    num = "";
+    numOne = "";
+    numTwo = "";
+    answer = "";
     disableButtons();
     calculate.disabled = true;
     equalNum.called = false;
